b5a073e90157382e1b82a544e29e37e068ff9977
MahirTanzim <tanzim15-5400@diu.edu.bd>
2025-06-24 20:31:09 +0600

    sd

---
+++ A_Buttons.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define bijli() (ios_base:: sync_with_stdio(false),cin.tie(NULL), cout.tie(NULL));
+#define ll long long int
+#define all(x) x.begin(),x.end()
+
+
+void solution(){
+    
+}
+
+
+int main(){
+    bijli();
+    int tc=1;
+    cin >> tc;
+    while(tc--){
+        solution();
+    }
+    return 0;
+}
