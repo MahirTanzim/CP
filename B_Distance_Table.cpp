f0ed56bdf4fe5f92c6381a3cc244b3bf25f07032
MahirTanzim <tanzim15-5400@diu.edu.bd>
2025-06-27 22:49:17 +0600

    asdf

---
+++ B_Distance_Table.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define bijli() (ios_base:: sync_with_stdio(false),cin.tie(NULL), cout.tie(NULL));
+# define endl "\n"
+# define ll long long int
+# define all(x) x.begin(),x.end()
+
+
+int main(){
+    bijli();
+     
+    
+    
+    return 0;
+}
