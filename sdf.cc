2e6cec468a993b9f34f834da2d7555d04c40efdb
MahirTanzim <tanzimm651@gmail.com>
2025-12-21 02:39:30 +0600

    ksjf

--- sdf.cc
+++ sdf.cc
@@ -1,47 +1,20 @@
-#include <iostream>
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-void printMatrix(int n) {
-    vector<vector<int>> matrix(n, vector<int>(n, 0));
-    int num = 1;
 
-    // Fill the matrix diagonally
-    for (int col = 0; col < n; col++) {
-        int i = 0, j = col;
-        while (i < n && j >= 0) {
-            matrix[i][j] = num++;
-            i++;
-            j--;
-        }
-    }
-
-    for (int row = 1; row < n; row++) {
-        int i = row, j = n - 1;
-        while (i < n && j >= 0) {
-            matrix[i][j] = num++;
-            i++;
-            j--;
-        }
-    }
-
-    // Print the matrix
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cout << matrix[i][j] << " ";
-        }
-        cout << endl;
-        cout << endl;
+void solution(){
+    int n = 5;
+    for(int i = 0; i < 5; i++){
+        cout << i+1 << endl;
     }
 }
 
-int main() {
-    int n;
-    // cout << "Enter the size of the matrix: ";
-    cin >> n;
 
-    printMatrix(n);
-
+int main(){
+    int tc = 1; 
+    cin >> tc;
+    while(tc--){
+        solution();
+    }
     return 0;
 }
