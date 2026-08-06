8f875bb4df170192845e26621574193647c3feaf
MahirTanzim <tanzimm651@gmail.com>
2026-08-06 23:31:08 +0600

    sdf

--- sdf.cc
+++ sdf.cc
@@ -4,19 +4,16 @@ using namespace std;
 
 void solution(){
     int n = 5;
-    for(int i = 0; i < 5; i++){
-        cout << i+1 << endl;
-    }
-    for(int i = 0; i <  5;  i++){
-        int a = 100 ;
+    cout << n << endl;
+    for(int i = 0; i < 10000000; i++) cout << i << endl; 
         
-    }
+    
 }
 
 
 int main(){
     int tc = 1; 
-    cin >> tc;
+    // cin >> tc;
     while(tc--){
         solution();
     }
